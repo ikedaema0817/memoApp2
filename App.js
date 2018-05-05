0f2b8473455ed8814b06bb13ac47dad238164e5b
前田啓 <maedakei@maedakeinoMacBook-puro.local>
2018-05-05 11:45:32 +0900

    memolist!

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar />
+        <MemoListScreen />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFDF6',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 78,
+  },
+});
---
+++ src/elements/CircleButton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class CircleButton extends React.Component {
+  render() {
+    return (
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonTitle}>
+          {this.props.children}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  circleButton: {
+    backgroundColor: '#E31676',
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  circleButtonTitle: {
+    fontSize: 24,
+    lineHeight: 24,
+    color: '#fff',
+  },
+});
+
+export default CircleButton;
